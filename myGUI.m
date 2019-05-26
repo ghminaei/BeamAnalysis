e2b673ed81a0abc972c1dd20064cf326f0259c1e
ghminaei <ghminaei1037@gmail.com>
2019-05-26 20:35:29 +0430

    GUI added

---
+++ myGUI.m
@@ -0,0 +1,160 @@
+function varargout = myGUI(varargin)
+% MYGUI MATLAB code for myGUI.fig
+%      MYGUI, by itself, creates a new MYGUI or raises the existing
+%      singleton*.
+%
+%      H = MYGUI returns the handle to a new MYGUI or the handle to
+%      the existing singleton*.
+%
+%      MYGUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MYGUI.M with the given input arguments.
+%
+%      MYGUI('Property','Value',...) creates a new MYGUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before myGUI_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to myGUI_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help myGUI
+
+% Last Modified by GUIDE v2.5 12-Jun-2018 18:45:19
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @myGUI_OpeningFcn, ...
+                   'gui_OutputFcn',  @myGUI_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before myGUI is made visible.
+function myGUI_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to myGUI (see VARARGIN)
+
+% Choose default command line output for myGUI
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes myGUI wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = myGUI_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in buttonRun.
+function buttonRun_Callback(hObject, eventdata, handles)
+% hObject    handle to buttonRun (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+    y= handles.inp;
+    guidata(gcbo,handles)
+    main(y)
+%     fid=fopen('outPut.txt','r')
+%     if fid~=1
+%         set(handles.textboxInp,'String',fscanf(fid,'%c'))
+%         fclose(fid)
+%     end
+    data1=importdata('outPut.txt','');
+    set(handles.edit3,'Max',2)
+    set(handles.edit3,'String',data1)
+    
+    
+    
+
+% --- Executes on key press with focus on buttonRun and none of its controls.
+function buttonRun_KeyPressFcn(hObject, eventdata, handles)
+% hObject    handle to buttonRun (see GCBO)
+% eventdata  structure with the following fields (see UICONTROL)
+%	Key: name of the key that was pressed, in lower case
+%	Character: character interpretation of the key(s) that was pressed
+%	Modifier: name(s) of the modifier key(s) (i.e., control, shift) pressed
+% handles    structure with handles and user data (see GUIDATA)
+    
+
+
+
+function textboxInp_Callback(hObject, eventdata, handles)
+% hObject    handle to textboxInp (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of textboxInp as text
+%        str2double(get(hObject,'String')) returns contents of textboxInp as a double
+    inp=get(hObject,'String')
+    handles.inp=inp
+    guidata(gcbo,handles)
+    
+
+% --- Executes during object creation, after setting all properties.
+function textboxInp_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to textboxInp (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+    
+
+
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in buttonN.
+
+
+
+function edit3_Callback(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit3 as text
+%        str2double(get(hObject,'String')) returns contents of edit3 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
